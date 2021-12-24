b5348a1708133447a9d8ef7c1310538197bb26ac
kholoud khraisat <kholoudmahmoud.kh@gmail.com>
2021-12-24 22:57:09 +0300

    contactform

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import ContactUs from "./components/contactUs/Contactus";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/contactus" element={<Contactus />} />
-          <Route path="/aboutus" element={<Aboutus />} />
+          {/* <Route path="/" element={<Landingpage />} /> */}
+          <Route path="/contactus" element={<ContactUs />} />
+          {/* <Route path="/aboutus" element={<Aboutus />} />
           <Route path="/listingcars" element={<Listingcars />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
