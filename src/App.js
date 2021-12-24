c8ca25fb28af38f5ab8367d357e0bb5d6cf2fa31
Shymaa-ashqar <shymaa.ashqar@gmail.com>
2021-12-24 21:11:10 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Listingcars from "./components/listingCars/Listingcars";
+import BookingForm from "./components/listingCars/BookingForm";
+import carsData from "./components/listingCars/carsData";
+import { useState } from "react";
 function App() {
+  const [cars_Data, setData] = useState(carsData);
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Landingpage />} />
+          {/* <Route path="/" element={<Landingpage />} />
           <Route path="/contactus" element={<Contactus />} />
-          <Route path="/aboutus" element={<Aboutus />} />
-          <Route path="/listingcars" element={<Listingcars />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/aboutus" element={<Aboutus />} /> */}
+          <Route
+            path="/listingcars"
+            element={<Listingcars cars={cars_Data} />}
+          />
+          <Route
+            path="/bookingForm/:id"
+            element={<BookingForm cars={cars_Data} />}
+          />
+          {/* <Route path="/Login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
