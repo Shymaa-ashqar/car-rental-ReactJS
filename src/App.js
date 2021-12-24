91e5877e0923137e1d6b4a2cfadba251e4857aac
beshiralkhadra <beshiralkhadra@gmail.com>
2021-12-24 12:54:28 +0200

    restructure folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1>hello teammates ,Beshir talking to you </h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/contactus" element={<Contactus />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/listingcars" element={<Listingcars />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+function Navbar({ submitted, logged, setLogged, setSubmitted }) {
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <div className="navbar">
+      <div className="logo-nav">
+        <Link to="/">
+          {" "}
+          <img src="" alt="logo" />
+        </Link>
+        <button className="burger-menu" onClick={() => setShowNav(!showNav)}>
+          <i className="fas fa-bars"></i>
+        </button>
+      </div>
+
+      <div className="nav-options">
+        <ul id={showNav ? "hidden" : null}>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/aboutus">About Us</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/listingcars">Listing Cars</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/contactus">Contact Us</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="register-btn">
+        <ul>
+          <li>
+            {!logged ? (
+              <Link to="/login">
+                <button className="login-btn">Login</button>
+              </Link>
+            ) : (
+              <Link to="/">
+                <button
+                  className="login-btn"
+                  onClick={() => {
+                    setLogged(false);
+                    setSubmitted(false);
+                  }}
+                >
+                  Logout
+                </button>
+              </Link>
+            )}
+          </li>
+          <li>
+            {!submitted ? (
+              <Link to="/signup">
+                <button className="signup-btn">Signup</button>
+              </Link>
+            ) : null}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
